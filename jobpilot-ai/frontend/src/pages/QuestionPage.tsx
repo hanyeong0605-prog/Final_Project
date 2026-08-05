176a098e9eff1b7fdda2750f1bcac243a5e183b1
me <oversb8@gmail.com>
2026-08-05 14:37:01 +0900

    껍데기

--- jobpilot-ai/frontend/src/pages/QuestionPage.tsx
+++ jobpilot-ai/frontend/src/pages/QuestionPage.tsx
@@ -1,4 +1,12 @@
-/*  커리어넷 검사 관련 API */ 
+/*  커리어넷 검사 관련 API */
 /* ( 성인 기준 : 직업 가치관 , 주요능력효능감 , 진로개발준비도 , 이공계전공적합도 ) */
+
+/* www.career.go.kr/inspct/openapi/test/questions?apikey=e54274d54718d12457fbb8a202f83d6e&q=6 (직업가치관검사 대학생/일반)
+www.career.go.kr/inspct/openapi/test/questions?apikey=e54274d54718d12457fbb8a202f83d6e&q=8 (진로개발준비도검사)
+www.career.go.kr/inspct/openapi/test/questions?apikey=e54274d54718d12457fbb8a202f83d6e&q=9 (이공계전공적합도
+www.career.go.kr/inspct/openapi/test/questions?apikey=e54274d54718d12457fbb8a202f83d6e&q=10 (주요능력효능감검사) */
+
+
+
 
 
